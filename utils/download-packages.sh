0ce0417152218c8784b3f32ad242494f365935ad
bouhaa <boukehaarsma23@gmail.com>
2023-06-12 23:44:38 +0200

    try 4

--- utils/download-packages.sh
+++ utils/download-packages.sh
@@ -6,7 +6,7 @@ set -x
 source /manifest;
 
 mkdir /nothing
-PIKAUR_CMD="pacman --dbpath /nothing --cachedir /output --noconfirm -Syw ${KERNEL_PACKAGE} ${KERNEL_PACKAGE}-headers ${PACKAGES}"
+PIKAUR_CMD="pacman --dbpath /nothing --cachedir /output --noconfirm -Syyw ${KERNEL_PACKAGE} ${KERNEL_PACKAGE}-headers ${PACKAGES}"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 "${PIKAUR_RUN[@]}"
 # remove any epoch (:) in name, replace with -- since not allowed in artifacts
